682cf0841fc5d4a5e69f1a8d6f2bdaffcf87e087
Andry <andrysadonis@gmail.com>
2022-11-27 11:27:15 -0500

    supabase values

---
+++ lib/utils/data_base_strings.dart
@@ -0,0 +1,5 @@
+class DataBaseStrings {
+  static const String supabaseUrl = 'https://lphcqkfxjeclrhxhtroj.supabase.co';
+  static const String supabaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwaGNxa2Z4amVjbHJoeGh0cm9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcxNjkzNjksImV4cCI6MTk4Mjc0NTM2OX0.1wsjjpL7yVUCt0Xeda_Lb6TysnbrhcXjG_Qiw9tuhB0';
+}

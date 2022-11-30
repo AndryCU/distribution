8ebdbc2ebc0905b905399ec9a7b938c2db4d77a2
Andry <andrysadonis@gmail.com>
2022-11-29 20:49:30 -0500

    update static text class

---
+++ lib/common/general_strings.dart
@@ -0,0 +1,7 @@
+class GeneralStrings {
+  static const cancelText = "Cancelar";
+  static const acceptText = "Aceptar";
+  static const addText = "Adicionar";
+  static const updateText = "Actualizar";
+  static const requiderValue = 'Valor requerido';
+}
---
+++ lib/features/core/strings_ui.dart
@@ -0,0 +1,13 @@
+class StringsUIEmployed {
+  static const deletedSuccessfullyText = "Borrado de forma existosa";
+  static const employeesNotFound = "No se han registrados trabajadores";
+  static const questionDeletedAlertDialog =
+      "¿Está seguro que desea borrar este empleado?";
+  static const male = 'Masculino';
+  static const female = 'Femenino';
+  static const intern = 'Interno';
+  static const extern = 'Externo';
+  static const errorMaxChildrens =
+      'La cantidad de hijos no puede ser mayor a 10';
+  static const selectSexError = 'Seleccione el sexo';
+}

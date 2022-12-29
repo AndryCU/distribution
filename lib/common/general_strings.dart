f78cef14463208fee6e3b386eed7cbeaef420e3e
Andry <andrysadonis@gmail.com>
2022-12-28 19:24:04 -0500

    updating messagues

--- lib/common/general_strings.dart
+++ lib/common/general_strings.dart
@@ -3,5 +3,7 @@ class GeneralStrings {
   static const acceptText = "Aceptar";
   static const addText = "Adicionar";
   static const updateText = "Actualizar";
-  static const requiderValue = 'Valor requerido';
+  static const requiredValue = 'Valor requerido';
+  static const regularExpressionText = "[a-zA-ZñÑáÁéÉíÍóÓúÚ ]";
+  static const connectionError = 'Error de conexión. Intentelo de nuevo';
 }

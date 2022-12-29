573d86f0269b4bbb1ad934171c054042baf3dc83
Andry <andrysadonis@gmail.com>
2022-12-29 10:04:16 -0500

    updating configuration page
    Added a file to manage all strings inside configuration page

--- lib/features/configuration/presentation/pages/configuration_page.dart
+++ lib/features/configuration/presentation/pages/configuration_page.dart
@@ -1,9 +1,18 @@
-import '../../../crud_employeed/presentation/state/riverpood.dart';
+import 'package:distribution/features/configuration/presentation/widgets/configurations_strings.dart';
+import 'package:distribution/features/crud_category/presentation/state/category_state.dart';
+import 'package:distribution/features/crud_employeed/presentation/state/riverpood.dart';
+import 'package:distribution/features/crud_products/presentation/state/riverpood_product.dart';
+import 'package:get_it/get_it.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+GetIt getIt = GetIt.instance;
+
 class ConfigurationPage extends ConsumerWidget {
+  const ConfigurationPage({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
@@ -11,10 +20,11 @@ class ConfigurationPage extends ConsumerWidget {
         actions: [
           Row(
             children: [
-              const Text('Actualizar'),
+              const Text(ConfigurationsStringsUI.updateGeneral),
               IconButton(
                 icon: const Icon(Icons.sync),
                 onPressed: () {
+                  ref.read(listProductController.notifier).syncProducts();
                   ref.read(listEmployedController.notifier).syncEmployees();
                 },
               ),
@@ -27,22 +37,23 @@ class ConfigurationPage extends ConsumerWidget {
         child: ListView(
           children: [
             ListTile(
-              title: const Text('Trabajadores'),
-              subtitle: const Text(
-                  'Gestione la creacion, modificacion y baja de los trabajadores'),
+              title: const Text(ConfigurationsStringsUI.employees),
+              subtitle:
+                  const Text(ConfigurationsStringsUI.employeesExplanation),
               trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                context.go('/settings_page/crud_employed_page');
-              },
+              onTap: () => context.go(ConfigurationsStringsUI.employeesRoute),
             ),
             ListTile(
-              title: const Text('Categoria'),
-              subtitle: const Text(
-                  'Gestione la creacion, modificacion y eliminacion de las categorias'),
+              title: const Text(ConfigurationsStringsUI.category),
+              subtitle: const Text(ConfigurationsStringsUI.categoryExplanation),
               trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                context.go('/settings_page/crud_categories_page');
-              },
+              onTap: () => context.go(ConfigurationsStringsUI.categoryRoute),
+            ),
+            ListTile(
+              title: const Text(ConfigurationsStringsUI.product),
+              subtitle: const Text(ConfigurationsStringsUI.productExplanation),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () => context.go(ConfigurationsStringsUI.productRoute),
             ),
           ],
         ),
---
+++ lib/features/configuration/presentation/widgets/configurations_strings.dart
@@ -0,0 +1,17 @@
+class ConfigurationsStringsUI {
+  static const updateGeneral = 'Actualizar todo';
+  static const employees = 'Trabajadores';
+  static const employeesExplanation =
+      'Gestione la creación, modificación y baja de los trabajadores';
+  static const category = 'Categorias';
+  static const categoryExplanation =
+      'Gestione la creación, modificación y eliminación de las categorias de productos';
+  static const product = 'Productos';
+  static const productExplanation =
+      'Gestione la creación, modificación y eliminación de los productos a distribuir';
+
+  //ROUTES
+  static const employeesRoute = '/settings_page/crud_employed_page';
+  static const categoryRoute = '/settings_page/crud_categories_page';
+  static const productRoute = '/settings_page/crud_products_page';
+}
